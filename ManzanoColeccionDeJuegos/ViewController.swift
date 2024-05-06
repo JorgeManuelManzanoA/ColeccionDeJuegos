365ea9d665e7a4ec3f27cbb1ae8020934ffb7533
jorgemanzano <jorge.manzano@tecsup.edu.pe>
2024-05-06 18:40:44 -0500

    Lab08

--- ManzanoColeccionDeJuegos/ViewController.swift
+++ ManzanoColeccionDeJuegos/ViewController.swift
@@ -9,15 +9,6 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let juego = juegos[indexPath.row]
-        cell.textLabel?.text = juego.titulo
-        cell.imageView?.image = UIImage(data: (juego.imagen!))
-        return cell
-    }
-    
-
     @IBOutlet weak var tableView: UITableView!
     var juegos : [Juego] = []
     
@@ -30,6 +21,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return juegos.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -39,6 +43,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         } catch {
         }
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
 }
 
